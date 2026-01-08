26740b9da973bcd3acd817d6c3ca55215a9ff0cf
Nandakumar Dhanenkula <nandakumar5353@gmail.com>
2026-01-08 17:50:04 +0530

    Implement StudentService for managing students

---
+++ Service: StudentService.java
@@ -0,0 +1,30 @@
+package com.yourname.sms.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.yourname.sms.entity.Student;
+import com.yourname.sms.repository.StudentRepository;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+}
